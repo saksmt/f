a10b1dbb73d71da5ac3878a1d79c38d804afbbde
Kirill Saksin <kirill.saksin@billing.ru>
2016-04-22 14:53:05 +0300

    Fixed pipe methods

--- functional/src/main/java/run/smt/f/functional/Pipes.java
+++ functional/src/main/java/run/smt/f/functional/Pipes.java
@@ -7,32 +7,39 @@ import run.smt.f.definition.procedure.Procedure0;
 import run.smt.f.definition.procedure.Procedure1;
 import run.smt.f.definition.procedure.Procedure2;
 
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import static run.smt.f.functional.Cast.f;
+
 /**
  * Contains combinations for reverse functional composition (piping)
  * @author Kirill Saksin <kirillsaksin@yandex.ru>
  */
 public interface Pipes {
-    static <R, RR> Function0<RR> pipe(Function0<R> f, Function1<R, RR> g) {
-        return f.andThen(g);
+    static <R, RR> Function0<RR> pipe(Supplier<R> f, Function<R, RR> g) {
+        return f(f).andThen(g);
     }
 
-    static <R> Procedure0 pipe(Function0<R> f, Procedure1<R> g) {
-        return f.andThen(g);
+    static <R> Procedure0 pipe(Supplier<R> f, Consumer<R> g) {
+        return f(f).andThen(g);
     }
 
-    static <A, R, RR> Function1<A, RR> pipe(Function1<A, R> f, Function1<R, RR> g) {
-        return f.andThen(g);
+    static <A, R, RR> Function1<A, RR> pipe(Function<A, R> f, Function<R, RR> g) {
+        return f(f).andThen(g);
     }
 
-    static <A, R> Procedure1<A> pipe(Function1<A, R> f, Procedure1<R> g) {
-        return f.andThen(g);
+    static <A, R> Procedure1<A> pipe(Function<A, R> f, Consumer<R> g) {
+        return f(f).andThen(g);
     }
 
-    static <A, B, R, RR> Function2<A, B, RR> pipe(Function2<A, B, R> f, Function1<R, RR> g) {
-        return f.andThen(g);
+    static <A, B, R, RR> Function2<A, B, RR> pipe(BiFunction<A, B, R> f, Function<R, RR> g) {
+        return f(f).andThen(g);
     }
 
-    static <A, B, R> Procedure2<A, B> pipe(Function2<A, B, R> f, Procedure1<R> g) {
-        return f.andThen(g);
+    static <A, B, R> Procedure2<A, B> pipe(BiFunction<A, B, R> f, Consumer<R> g) {
+        return f(f).andThen(g);
     }
 }

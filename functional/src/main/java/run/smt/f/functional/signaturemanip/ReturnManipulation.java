81caed91dca61aab590fffac7df4313f1fa6c06b
Kirill Saksin <kirill.saksin@billing.ru>
2016-04-22 14:48:46 +0300

    Fixed return manipulation methods

--- functional/src/main/java/run/smt/f/functional/signaturemanip/ReturnManipulation.java
+++ functional/src/main/java/run/smt/f/functional/signaturemanip/ReturnManipulation.java
@@ -7,33 +7,37 @@ import run.smt.f.definition.procedure.Procedure0;
 import run.smt.f.definition.procedure.Procedure1;
 import run.smt.f.definition.procedure.Procedure2;
 
+import java.util.function.*;
+
+import static run.smt.f.functional.Cast.f;
+
 /**
  * Contains functions for providing and suppressing return of functions
  * @author Kirill Saksin <kirillsaksin@yandex.ru>
  */
 public interface ReturnManipulation {
-    static <R> Function0<R> provideReturn(Procedure0 f, R value) {
-        return f.provideReturn(value);
+    static <R> Function0<R> provideReturn(Runnable f, R value) {
+        return f(f).provideReturn(value);
     }
 
-    static <A, R> Function1<A, R> provideReturn(Procedure1<A> f, R value) {
-        return f.provideReturn(value);
+    static <A, R> Function1<A, R> provideReturn(Consumer<A> f, R value) {
+        return f(f).provideReturn(value);
     }
 
-    static <A, B, R> Function2<A, B, R> provideReturn(Procedure2<A, B> f, R value) {
-        return f.provideReturn(value);
+    static <A, B, R> Function2<A, B, R> provideReturn(BiConsumer<A, B> f, R value) {
+        return f(f).provideReturn(value);
     }
 
 
-    static Procedure0 suppressReturn(Function0<?> f) {
-        return f.suppressReturn();
+    static Procedure0 suppressReturn(Supplier<?> f) {
+        return f(f).suppressReturn();
     }
 
-    static <A> Procedure1<A> suppressReturn(Function1<A, ?> f) {
-        return f.suppressReturn();
+    static <A> Procedure1<A> suppressReturn(Function<A, ?> f) {
+        return f(f).suppressReturn();
     }
 
-    static <A, B> Procedure2<A, B> suppressReturn(Function2<A, B, ?> f) {
-        return f.suppressReturn();
+    static <A, B> Procedure2<A, B> suppressReturn(BiFunction<A, B, ?> f) {
+        return f(f).suppressReturn();
     }
 }
